f4ebcd6cf384e6fd83c1e78d8171882095d25076
Igor Souza <igormcsouza@gmail.com>
2024-04-11 13:40:00 +0000

    Update styling of the mdx components
    Now there is a new type of Callout, the `informative`, and the dark mode
    was fixed.
    
    Also the behavior of the title link changed, before we had it underline,
    now it adds a small hashtag at the end of it indicating that we can click
    and get the link to that title.

--- components/callout.tsx
+++ components/callout.tsx
@@ -3,7 +3,7 @@ import { cn } from "@/lib/utils"
 interface CalloutProps {
   icon?: string
   children?: React.ReactNode
-  type?: "default" | "warning" | "danger"
+  type?: "default" | "warning" | "danger" | "informative"
 }
 
 export function Callout({
@@ -15,8 +15,9 @@ export function Callout({
   return (
     <div
       className={cn("my-6 flex items-start rounded-md border border-l-4 p-4", {
-        "border-red-900 bg-red-50": type === "danger",
-        "border-yellow-900 bg-yellow-50": type === "warning",
+        "border-red-900 bg-red-50 dark:bg-transparent": type === "danger",
+        "border-yellow-900 bg-yellow-50 dark:bg-transparent": type === "warning",
+        "border-green-900 bg-green-50 dark:bg-transparent": type === "informative",
       })}
       {...props}
     >
